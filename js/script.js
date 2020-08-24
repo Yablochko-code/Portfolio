ac3959122b253f103f38d377f81f63824515977d
Dima <57165970+Yablochko-code@users.noreply.github.com>
2020-08-24 16:18:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
